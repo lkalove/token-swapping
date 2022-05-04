e3231a9685f0a62919f521f37dbb6587f776db5e
Kyunga <whitelka@gmail.com>
2022-05-05 02:08:23 +0900

    Update App.vue
    set type of className

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'App',
   methods: {
-    initBodyClass(className) {
+    initBodyClass(className: string) {
       document.body.setAttribute('class', className);
     },
     setBodyClass() {
